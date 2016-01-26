744e55a264b0c6012ffbe18e3e28a141f3802b20
mrf <klamparski@gmail.com>
2016-01-26 01:39:58 +0100

    Refactor and push functionality implement

--- Classes/Mrf/Synchro/Command/SynchroCommandController.php
+++ Classes/Mrf/Synchro/Command/SynchroCommandController.php
@@ -3,18 +3,27 @@
 namespace Mrf\Synchro\Command;
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Cli\CommandController;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+use TYPO3\Surf\Domain\Model\Node;
 use TYPO3\Surf\Exception\TaskExecutionException;
 
 /**
  * @Flow\Scope("singleton")
  */
-class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
+class SynchroCommandController extends CommandController
 {
 
     /**
      * Name of the temporary subdirectory in FLOW_PATH_DATA
      */
     const DATA_DIRECTORY_NAME = 'Synchro';
+
+    /**
+     * Name of the exported XML document
+     */
+    const EXPORT_XML_FILENAME = 'export.xml';
 
     /**
      * @Flow\Inject
@@ -73,10 +82,10 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
     {
         try {
             $this->initializeDeployment($deploymentName, $configurationPath);
-            $this->log('Executing database and resources synchronization.');
-            $this->run();
+            $this->log('Executing database and resources pull');
+            $this->runPull();
         } catch (TaskExecutionException $e) {
-            $this->log('An error occurred during the synchronization: '.$e->getMessage(), LOG_ERR);
+            $this->log('An error occurred during the pull: '.$e->getMessage(), LOG_ERR);
         }
     }
 
@@ -86,21 +95,59 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
      *
      * @return void
      */
-    public function simulateCommand($deploymentName, $configurationPath = null)
+    public function simulatePullCommand($deploymentName, $configurationPath = null)
     {
         try {
             $this->initializeDeployment($deploymentName, $configurationPath, LOG_DEBUG);
             $this->deployment->setDryRun(true);
-            $this->log('Simulating database and resources synchronization.');
-            $this->run();
+            $this->log('Simulating database and resources pull');
+            $this->runPull();
         } catch (TaskExecutionException $e) {
-            $this->log('An error occurred during the simulation of the synchronization: '.$e->getMessage(), LOG_ERR);
+            $this->log('An error occurred during the simulation of the pull: '.$e->getMessage(), LOG_ERR);
         }
     }
 
     /**
      * @param string $deploymentName The deployment name
      * @param string $configurationPath Path for deployment configuration files
+     *
+     * @return void
+     *
+     * @throws TaskExecutionException
+     */
+    public function pushCommand($deploymentName, $configurationPath = null)
+    {
+        try {
+            $this->initializeDeployment($deploymentName, $configurationPath);
+            $this->log('Executing database and resources push');
+            $this->runPush();
+        } catch (TaskExecutionException $e) {
+            $this->log('An error occurred during the push: '.$e->getMessage(), LOG_ERR);
+        }
+    }
+
+    /**
+     * @param string $deploymentName The deployment name
+     * @param string $configurationPath Path for deployment configuration files
+     *
+     * @return void
+     */
+    public function simulatePushCommand($deploymentName, $configurationPath = null)
+    {
+        try {
+            $this->initializeDeployment($deploymentName, $configurationPath, LOG_DEBUG);
+            $this->deployment->setDryRun(true);
+            $this->log('Simulating database and resources push.');
+            $this->runPush();
+        } catch (TaskExecutionException $e) {
+            $this->log('An error occurred during the simulation of the push: '.$e->getMessage(), LOG_ERR);
+        }
+    }
+
+    /**
+     * @param string $deploymentName The deployment name
+     * @param string $configurationPath Path for deployment configuration files
+     * @param int $logLevel
      *
      * @return void
      *
@@ -120,7 +167,7 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
      *
      * @throws TaskExecutionException
      */
-    protected function run()
+    protected function runPull()
     {
         $this->setIdentifier();
         $this->setApplication($this->deployment);
@@ -128,17 +175,51 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
         $this->setLocalNode();
 
         // export site on remote node
-        $exportFile = $this->exportRemoteSite();
+        $this->createRemoteDataSynchroDirectory();
+        $this->exportRemoteSite();
 
         // download site to local
-        $this->createLocalDataSynchroDir();
-        $this->downloadFiles(
-            dirname($exportFile).'/',
-            $this->getDownloadPath()
+        $this->createLocalDataSynchroDirectory();
+        $this->download(
+            $this->getRemoteDataSynchroDirectory(),
+            $this->getLocalDataSynchroDirectory()
         );
 
         // import site
-        $this->importSite($this->getDownloadPath().basename($exportFile));
+        $this->pruneLocalSite();
+        $this->importSiteLocally();
+        $this->clearLocalCache();
+
+        $this->response->setExitCode(0);
+    }
+
+    /**
+     * @return void
+     *
+     * @throws TaskExecutionException
+     */
+    protected function runPush()
+    {
+        $this->setIdentifier();
+        $this->setApplication($this->deployment);
+        $this->setRemoteNode($this->application);
+        $this->setLocalNode();
+
+        // export site on local node
+        $this->createLocalDataSynchroDirectory();
+        $this->exportLocalSite();
+
+        // upload site to remote
+        $this->createRemoteDataSynchroDirectory();
+        $this->upload(
+            $this->getLocalDataSynchroDirectory(),
+            $this->getRemoteDataSynchroDirectory()
+        );
+
+        // import site remotely
+        $this->pruneRemoteSite();
+        $this->importSiteRemotely();
+        $this->clearRemoteCache();
 
         $this->response->setExitCode(0);
     }
@@ -151,6 +232,7 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
     protected function setIdentifier()
     {
         $this->identifier = sha1(uniqid().time());
+        $this->log('Identifier: '.$this->identifier);
     }
 
     /**
@@ -160,7 +242,7 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
      *
      * @throws TaskExecutionException
      */
-    protected function setApplication(\TYPO3\Surf\Domain\Model\Deployment $deployment)
+    protected function setApplication(Deployment $deployment)
     {
         $applications = $deployment->getApplications();
 
@@ -170,7 +252,6 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
             );
         }
 
-        /** @var \TYPO3\Surf\Domain\Model\Application $application */
         $this->application = array_shift($applications);
     }
 
@@ -181,8 +262,10 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
      *
      * @throws TaskExecutionException
      */
-    protected function setRemoteNode(\TYPO3\Surf\Domain\Model\Application $application)
+    protected function setRemoteNode(Application $application)
     {
+        $this->requireApplication(__METHOD__);
+
         $nodes = $application->getNodes();
 
         if (count($nodes) < 1) {
@@ -191,7 +274,6 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
             );
         }
 
-        /** @var \TYPO3\Surf\Domain\Model\Node $node */
         $this->remoteNode = array_shift($nodes);
     }
 
@@ -200,31 +282,200 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
      */
     protected function setLocalNode()
     {
-        $this->localNode = new \TYPO3\Surf\Domain\Model\Node('localhost');
+        $this->localNode = new Node('localhost');
         $this->localNode->setHostname('localhost');
     }
 
     /**
-     * Exports the site using Flow site:export command
+     * @param string $callingMethodName
      *
-     * @return string Remote path to the exported site file
+     * @return void
+     *
+     * @throws TaskExecutionException
+     */
+    protected function requireIdentifier($callingMethodName)
+    {
+        if (!$this->identifier) {
+            throw new TaskExecutionException(
+                'Identifier has to be set before executing method '.$callingMethodName.'.'
+            );
+        }
+    }
+
+    /**
+     * @param string $callingMethodName
+     *
+     * @return void
+     *
+     * @throws TaskExecutionException
+     */
+    protected function requireApplication($callingMethodName)
+    {
+        if (!$this->application) {
+            throw new TaskExecutionException(
+                'Application has to be set before executing method '.$callingMethodName.'.'
+            );
+        }
+    }
+
+    /**
+     * @return string Remote context name
+     */
+    protected function getRemoteContext()
+    {
+        $this->requireApplication(__METHOD__);
+
+        return $this->application->getContext();
+    }
+
+    /**
+     * @return string Local context name
+     */
+    protected function getLocalContext()
+    {
+        return getenv('FLOW_CONTEXT');
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRemoteRootPath()
+    {
+        $this->requireApplication(__METHOD__);
+
+        return $this->application->getDeploymentPath().'/releases/current/';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getLocalRootPath()
+    {
+        return FLOW_PATH_ROOT;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRemoteDataSynchroDirectory()
+    {
+        $this->requireIdentifier(__METHOD__);
+
+        return $this->getRemoteRootPath().'Data/'.self::DATA_DIRECTORY_NAME.'/'.$this->identifier.'/';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getLocalDataSynchroDirectory()
+    {
+        $this->requireIdentifier(__METHOD__);
+
+        return FLOW_PATH_DATA.self::DATA_DIRECTORY_NAME.DIRECTORY_SEPARATOR.$this->identifier.DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRemoteExportXmlFilename()
+    {
+        return $this->getRemoteDataSynchroDirectory().self::EXPORT_XML_FILENAME;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getLocalExportXmlFilename()
+    {
+        return $this->getLocalDataSynchroDirectory().self::EXPORT_XML_FILENAME;
+    }
+
+    /**
+     * Creates Data/Synchro/[identifier] directory on remote node
+     *
+     * @return void
+     */
+    protected function createRemoteDataSynchroDirectory()
+    {
+        $this->requireRemoteNode(__METHOD__);
+
+        $this->log('Creating remote synchro directory');
+        $this->shell->executeOrSimulate(
+            'mkdir -p -m 777 '.$this->getRemoteDataSynchroDirectory(),
+            $this->remoteNode,
+            $this->deployment
+        );
+    }
+
+    /**
+     * Creates Data/Synchro/[identifier] directory on local node
+     *
+     * @return void
+     */
+    protected function createLocalDataSynchroDirectory()
+    {
+        $this->requireLocalNode(__METHOD__);
+
+        $this->log('Creating local synchro directory');
+        $this->shell->executeOrSimulate(
+            'mkdir -p -m 777 '.$this->getLocalDataSynchroDirectory(),
+            $this->localNode,
+            $this->deployment
+        );
+    }
+
+    /**
+     * @param string $callingMethodName
+     *
+     * @return void
+     *
+     * @throws TaskExecutionException
+     */
+    protected function requireRemoteNode($callingMethodName)
+    {
+        if (!$this->remoteNode) {
+            throw new TaskExecutionException(
+                'Remote node has to be set before executing method '.$callingMethodName.'.'
+            );
+        }
+    }
+
+    /**
+     * @param string $callingMethodName
+     *
+     * @return void
+     *
+     * @throws TaskExecutionException
+     */
+    protected function requireLocalNode($callingMethodName)
+    {
+        if (!$this->localNode) {
+            throw new TaskExecutionException(
+                'Local node has to be set before executing method '.$callingMethodName.'.'
+            );
+        }
+    }
+
+    /**
+     * Exports the remote node's site using Flow site:export command
      *
      * @throws TaskExecutionException
      */
     protected function exportRemoteSite()
     {
-        $currentPath = $this->application->getDeploymentPath().'/current';
-        $dataSynchroDir = $currentPath.'/Data/'.self::DATA_DIRECTORY_NAME.'/'.$this->identifier; // on remote dir we don't use DIRECTORY_SEPARATOR constant
-        $exportFilename = $dataSynchroDir.'/export_'.$this->identifier.'.xml';
+        $this->log('Exporting remote site');
+        $this->flowRemoteCommand('site:export --filename '.$this->getRemoteExportXmlFilename());
+    }
 
-        $cmds = array(
-            "mkdir -p {$dataSynchroDir}",
-            "FLOW_CONTEXT={$this->application->getContext()} {$currentPath}/flow site:export --filename {$exportFilename}",
-        );
-
-        $this->shell->executeOrSimulate(implode(' && ', $cmds), $this->remoteNode, $this->deployment);
-
-        return $exportFilename;
+    /**
+     * Exports the local node's site using Flow site:export command
+     *
+     * @throws TaskExecutionException
+     */
+    protected function exportLocalSite()
+    {
+        $this->log('Exporting local site');
+        $this->flowLocalCommand('site:export --filename '.$this->getLocalExportXmlFilename());
     }
 
     /**
@@ -233,7 +484,7 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
      * @param string $remotePath
      * @param string $localPath
      */
-    protected function downloadFiles($remotePath, $localPath)
+    protected function download($remotePath, $localPath)
     {
         $cmd = sprintf(
             'rsync  -e "ssh -p %d" -avz %s@%s:%s %s',
@@ -243,55 +494,110 @@ class SynchroCommandController extends \TYPO3\Flow\Cli\CommandController
             $remotePath,
             $localPath
         );
-
+        $this->log('Downloading files to local instance');
         $this->shell->executeOrSimulate($cmd, $this->localNode, $this->deployment);
     }
 
     /**
-     * @return string
-     *
-     * @throws TaskExecutionException
+     * @param string $localPath
+     * @param string $remotePath
      */
-    protected function getDownloadPath()
+    protected function upload($localPath, $remotePath)
     {
-        if (empty($this->identifier)) {
-            throw new TaskExecutionException('Identifier has to be set to get download path');
-        }
+        $cmd = sprintf(
+            'rsync  -e "ssh -p %d" -avz %s %s@%s:%s',
+            $this->remoteNode->getOption('port'),
+            $localPath,
+            $this->remoteNode->getOption('username'),
+            $this->remoteNode->getHostname(),
+            $remotePath
+        );
+        $this->log('Uploading files to remote instance');
+        $this->shell->executeOrSimulate($cmd, $this->localNode, $this->deployment);
+    }
 
-        return FLOW_PATH_DATA.self::DATA_DIRECTORY_NAME.DIRECTORY_SEPARATOR.$this->identifier.DIRECTORY_SEPARATOR;
+    /**
+     * Imports the site on the local node using Flow site:import command
+     *
+     * @return void
+     */
+    protected function importSiteLocally()
+    {
+        $this->log('Importing site on the local instance');
+        $this->flowLocalCommand('site:import --filename '.$this->getLocalExportXmlFilename());
+    }
+
+    /**
+     * Imports the site on the remote instance using Flow site:import command
+     *
+     * @return void
+     */
+    protected function importSiteRemotely()
+    {
+        $this->log('Importing site on the remote instance');
+        $this->flowRemoteCommand('site:import --filename '.$this->getRemoteExportXmlFilename());
     }
 
     /**
      * @return void
      */
-    protected function createLocalDataSynchroDir()
+    protected function pruneLocalSite()
     {
-        if (!is_dir($this->getDownloadPath())) {
-            $this->shell->executeOrSimulate(
-                'mkdir -p -m 777 '.$this->getDownloadPath(),
-                $this->localNode,
-                $this->deployment
-            );
-        }
+        $this->log('Pruning of the local instance');
+        $this->flowLocalCommand('site:prune');
     }
 
     /**
-     * Imports the site using Flow site:import command
-     *
      * @return void
      */
-    protected function importSite($filename)
+    protected function pruneRemoteSite()
     {
+        $this->log('Pruning of the remote instance');
+        $this->flowRemoteCommand('site:prune');
+    }
+
+    /**
+     * @return void
+     */
+    protected function clearLocalCache()
+    {
+        $this->log('Clearing cache of the local instance');
+        $this->flowLocalCommand('flow:cache:flush --force');
+    }
+
+    /**
+     * @return void
+     */
+    protected function clearRemoteCache()
+    {
+        $this->log('Clearing cache of the remote instance');
+        $this->flowRemoteCommand('flow:cache:flush --force');
+    }
+
+    /**
+     * @param string $command
+     */
+    protected function flowLocalCommand($command) {
         $cmd = array();
-
-        // depending on configuration, FLOW_CONTEXT may be required for connection to database
-        $context = getenv('FLOW_CONTEXT');
+        $context = $this->getLocalContext();
         if ($context) {
-            $cmd[] = "FLOW_CONTEXT={$context}";
+            $cmd[] = 'FLOW_CONTEXT='.$context;
         }
-        $cmd[] = FLOW_PATH_ROOT.DIRECTORY_SEPARATOR."flow site:import --filename {$filename}";
-
+        $cmd[] = $this->getLocalRootPath().'flow '.$command;
         $this->shell->executeOrSimulate(implode(' ', $cmd), $this->localNode, $this->deployment);
+    }
+
+    /**
+     * @param string $command
+     */
+    protected function flowRemoteCommand($command) {
+        $cmd = array();
+        $context = $this->getRemoteContext();
+        if ($context) {
+            $cmd[] = 'FLOW_CONTEXT='.$context;
+        }
+        $cmd[] = $this->getRemoteRootPath().'flow '.$command;
+        $this->shell->executeOrSimulate(implode(' ', $cmd), $this->remoteNode, $this->deployment);
     }
 
     /**
